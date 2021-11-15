31357583791e9222aa552d8001d8e99dd1bc4e74
ghslada <189435@upf.br>
2021-11-15 20:59:54 -0300

    'finalizado_update_de_usuarios'

--- listas/gerarListaUsers.php
+++ listas/gerarListaUsers.php
@@ -8,9 +8,9 @@ function gerarListaUsers(){
     if(isset($result)){
         foreach($result as $res){
             if($res['tipo_usuario_id']==1){
-                $res['tipo_usuario_id']="Cliente";
+                // $res['tipo_usuario_id']="Cliente";
             }else if($res['tipo_usuario_id']==2){
-                $res['tipo_usuario_id']="Administrador";
+                // $res['tipo_usuario_id']="Administrador";
             }else{
                 $res['tipo_usuario_id']="ERRO.";
             }
@@ -25,7 +25,7 @@ function gerarListaUsers(){
                         <td>{$res['cidade_cep']}</td>
                         <td>{$res['tipo_usuario_id']}</td>
                         <td>{$res['telefone']}</td>
-                        <td><a href='../updates/updateUsuario.php?cpf={$res['cpf']}&nome={$res['nome']}&email={$res['nome']}&senha={$res['senha']}&endereco={$res['endereco']}&cidade_cep={$res['endereco']}&tipo_usuario_id={$res['endereco']}&telefone={$res['telefone']}' class='btn btn-warning' type=submit name='alterar'><i class='bi bi-pencil-square'></i></a></td>
+                        <td><a href='./formAlterarUsuario.php?cpf={$res['cpf']}&nome={$res['nome']}&email={$res['email']}&senha={$res['senha']}&endereco={$res['endereco']}&cep={$res['cidade_cep']}&tipo={$res['tipo_usuario_id']}&telefone={$res['telefone']}' class='btn btn-warning' type=submit name='alterar'><i class='bi bi-pencil-square'></i></a></td>
                         <td><div class='btn btn-danger'><i class='bi bi-person-x'></i></div></td>
                     </tr>
                 </form>");
